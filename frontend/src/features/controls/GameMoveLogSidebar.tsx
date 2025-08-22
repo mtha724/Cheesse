ff10bde7969eced7d7fe05287582126387d4c136
macy <macybutler2004@gmail.com>
2025-08-22 15:20:46 +1200

    feat: added menu boxes for move log and game buttons, also made the pieces bigger and added drop shadows, also made the images pixellated

---
+++ frontend/src/features/controls/GameMoveLogSidebar.tsx
@@ -0,0 +1,15 @@
+import './GameMoveLogSidebar.css'
+
+export default function GameMoveLogSidebar() {
+    return (
+     <div className="game-log-wrapper">
+      <div className="game-move-log-sidebar">
+        <h1 className="move-log-lbl">Move Log</h1>
+        <div className="move-log-wrapper">
+          {/*added move log here*/}
+          <p>Move log element when we make it</p>
+        </div>
+      </div>
+    </div>
+  );
+}
